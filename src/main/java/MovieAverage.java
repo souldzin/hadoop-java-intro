0ffa13ee28dd97842cbfde01d5387a8627589edf
souldzin <souldzin+git@souldzin.com>
2018-02-03 17:04:27 -0600

    add MovieAverage and renamed project

--- src/main/java/MovieAverage.java
+++ src/main/java/MovieAverage.java
@@ -3,11 +3,10 @@
  * (c) Someone at sometime
  */
 import java.io.IOException;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -17,50 +16,44 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class WordCount {
+public class MovieAverage {
     public static class Map extends Mapper<Object, Text, Text, IntWritable> {
-        // consts
-        // ==============
-        private final static IntWritable one = new IntWritable(1);
-        private final static Pattern pattern = createWordPattern();
-
-        private static Pattern createWordPattern() {
-            // Let's look for words:
-            // - they must start and end with a word character
-            // - they can contain one of the [-'"] characters in the middle
-            return Pattern.compile("(\\w+)([-'\"]\\w+)*");
-        }
-
         // instance vars
         // ==============
-        private Text word = new Text();
+        private Text movieID = new Text();
+        private IntWritable rating = new IntWritable();
 
         // instance methods
         // ==============
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            Matcher matcher = pattern.matcher(value.toString());
-
-            while(matcher.find()) {
-                word.set(matcher.group());
-
-                context.write(word, one);
+            try {
+                String[] row = value.toString().split(" ");
+                movieID.set(row[0]);
+                rating.set(Integer.parseInt(row[1]));
+                context.write(movieID, rating);
+            } catch(NumberFormatException e) {
+                // oh no! parseInt failed!
+                // we'll just ignore this for now...
+                System.out.println("Parse int failed...");
             }
         }
     }
 
-    public static class Reduce extends Reducer<Text,IntWritable,Text,IntWritable> {
+    public static class Reduce extends Reducer<Text,IntWritable,Text,DoubleWritable> {
         // instance
         // =============
-        private IntWritable result = new IntWritable();
+        private DoubleWritable result = new DoubleWritable();
 
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            int sum = 0;
+            long sum = 0;
+            long count = 0;
 
             for (IntWritable val : values) {
                 sum += val.get();
+                count += 1;
             }
 
-            result.set(sum);
+            result.set(sum/(double)count);
             context.write(key, result);
         }
     }
@@ -73,16 +66,15 @@ public class WordCount {
 
         // get all args
         if (otherArgs.length != 2) {
-            System.err.println("Usage: WordCount <in> <out>");
+            System.err.println("Usage: MovieAverage <in> <out>");
             System.exit(2);
         }
 
         // setup job classes / configuration
-        Job job = Job.getInstance(conf, "wordcount");
+        Job job = Job.getInstance(conf, "movieavg");
         job.setJarByClass(WordCount.class);
         job.setMapperClass(Map.class);
         job.setReducerClass(Reduce.class);
-        job.setCombinerClass(Reduce.class);
 
         // set job output types
         job.setOutputKeyClass(Text.class);
