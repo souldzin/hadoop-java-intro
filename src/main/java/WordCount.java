99af9886bd2740ccf7fb3fad30a96a99ad59f762
souldzin <souldzin+git@souldzin.com>
2018-02-03 14:49:42 -0600

    cleanup

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,27 +1,47 @@
+/**
+ * This code was taken from an in-class example
+ * (c) Someone at sometime
+ */
 import java.io.IOException;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class WordCount {
-    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+    public static class Map extends Mapper<Object, Text, Text, IntWritable> {
+        // consts
+        // ==============
         private final static IntWritable one = new IntWritable(1);
+        private final static Pattern pattern = createWordPattern();
 
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            Pattern pattern = Pattern.compile("(\\w+)([-'\"]\\w+)*");
+        private static Pattern createWordPattern() {
+            // Let's look for words:
+            // - they must start and end with a word character
+            // - they can contain one of the [-'"] characters in the middle
+            return Pattern.compile("(\\w+)([-'\"]\\w+)*");
+        }
+
+        // instance vars
+        // ==============
+        private Text word = new Text();
+
+        // instance methods
+        // ==============
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             Matcher matcher = pattern.matcher(value.toString());
+
             while(matcher.find()) {
-                Text word = new Text(matcher.group());
+                word.set(matcher.group());
 
                 context.write(word, one);
             }
@@ -29,19 +49,24 @@ public class WordCount {
     }
 
     public static class Reduce extends Reducer<Text,IntWritable,Text,IntWritable> {
+        // instance
+        // =============
         private IntWritable result = new IntWritable();
 
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            int sum = 0; // initialize the sum for each keyword
+            int sum = 0;
+
             for (IntWritable val : values) {
                 sum += val.get();
             }
+
             result.set(sum);
-            context.write(key, result); // create a pair <keyword, number of occurences>
+            context.write(key, result);
         }
     }
 
-    // Driver program
+    // main
+    // =============
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
@@ -52,25 +77,19 @@ public class WordCount {
             System.exit(2);
         }
 
-        // create a job with name "wordcount"
-        Job job = new Job(conf, "wordcount");
+        // setup job classes / configuration
+        Job job = Job.getInstance(conf, "wordcount");
         job.setJarByClass(WordCount.class);
         job.setMapperClass(Map.class);
         job.setReducerClass(Reduce.class);
-
-        // uncomment the following line to add the Combiner
         job.setCombinerClass(Reduce.class);
 
-        // set output key type
+        // set job output types
         job.setOutputKeyClass(Text.class);
-
-        // set output value type
         job.setOutputValueClass(IntWritable.class);
 
-        //set the HDFS path of the input data
+        // set I/O
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-
-        // set the HDFS path for the output
         FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 
         //Wait till job completion
